5a3b0d8ac726a750b5116d02509c6788243bf46c
Mateusz Krupa <matkrupa@gmail.com>
2023-11-13 00:34:54 +0100

    Adding portfolio name

--- src/components/AssetsDrawer.tsx
+++ src/components/AssetsDrawer.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-import {
-  Drawer,
-} from "@material-tailwind/react";
+import React, { Fragment } from "react";
+import { Drawer } from "@material-tailwind/react";
 import AssetsTable from "./AssetsTable";
 import { Portfolio } from "./types";
 
@@ -14,27 +12,36 @@ interface IClientDetailsDrawerProps {
 const AssetsDrawer: React.FC<IClientDetailsDrawerProps> = ({
   portfolios,
   close,
-  open
+  open,
 }) => {
-
-  // this is a hack to fix the bug in the library - and it is a little to late to change it now 
-  if(!open) return null;
+  // this is a hack to fix the bug in the library - and it is a little to late to change it now
+  if (!open) return null;
   return (
     <>
       <Drawer
-        //Ignoring next line  
-        //This is a bug in the library - it allows you yo put only number but strings also works 
-        // i this case it will be more mobile friendly as user will be able to scroll down and close 
+        //Ignoring next line
+        //This is a bug in the library - it allows you yo put only number but strings also works
+        // i this case it will be more mobile friendly as user will be able to scroll down and close
         // the drawer by clicking on the background
         //@ts-ignore-next-line
         size={"50vh"}
         open={open}
         onClose={close}
-        className="p-4 w-full bg-white"
+        className="p-4 w-full bg-white overflow-y-scroll"
         placement="bottom"
       >
         {portfolios.map((portfolio) => {
-          return <AssetsTable assets={portfolio.assets} key={portfolio.portfolioId} />;
+          return (
+            <div key={portfolio.portfolioId} >
+              <h3 className="text-md font-semibold pt-4 px-4">
+                Portfolio name: {portfolio.portfolioName}
+              </h3>
+              <AssetsTable
+                assets={portfolio.assets}
+                key={portfolio.portfolioId}
+              />
+            </div>
+          );
         })}
       </Drawer>
     </>
